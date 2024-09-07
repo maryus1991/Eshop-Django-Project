e2fb729ff7fde53a11b027c95a2cd048dda70cb1
maryus1991 <77290377+maryus1991@users.noreply.github.com>
2024-09-07 23:41:46 +0330

    Update settings.py
    removing some private staff

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -101,7 +101,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'mail.mebrahimy.ir'
+EMAIL_HOST = ''
 EMAIL_HOST_USER = ' '
 EMAIL_HOST_PASSWORD = ' '
 EMAIL_PORT = 587
